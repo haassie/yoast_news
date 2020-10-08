33eede1d76cd2310e37c9eb2953c603f1a8fc8d8
Richard Vollebregt <richard.vollebregt@maxserv.com>
2020-10-08 10:43:28 +0200

    [BUGFIX] add missing definitions in SQL for fields that are defined in TCA

--- ext_tables.sql
+++ ext_tables.sql
@@ -9,4 +9,7 @@ CREATE TABLE tx_news_domain_model_news (
   twitter_title varchar(255) DEFAULT '' NOT NULL,
   twitter_description text,
   twitter_image int(11) unsigned DEFAULT '0' NOT NULL,
+  tx_yoastseo_snippetpreview tinyint(3) DEFAULT '0' NOT NULL,
+  tx_yoastseo_readability_analysis tinyint(3) DEFAULT '0' NOT NULL,
+  tx_yoastseo_focuskeyword_analysis tinyint(3) DEFAULT '0' NOT NULL
 );
